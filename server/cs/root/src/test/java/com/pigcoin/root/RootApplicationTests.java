70504cc823fa63d2154ea7727cdd6d2d48aab12d
SuperMax <aamyj@vip.qq.com>
2021-03-26 11:26:15 +0800

    update   :   update   community   server

---
+++ server/cs/root/src/test/java/com/pigcoin/root/RootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pigcoin.root;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
